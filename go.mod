01468e95bc111854718e3d29ccb4afca7db1a339
Fan Yang <me@minidump.info>
2020-04-07 13:42:16 +0800

    (+)add files

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/missdeer/msteams-auth
+
+go 1.13
+
+require github.com/gin-gonic/gin v1.6.2
---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	code string
+)
+
+type AccessTokenRequestBody struct {
+	GrantType   string `json:"grant_type"`
+	ClientID    string `json:"client_id"`
+	Scope       string `json:"scope"`
+	Code        string `json:"code"`
+	RedirectURI string `json:"redirect_uri"`
+}
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.Redirect(http.StatusFound, "https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=46442420-1b26-4bd7-a997-183e1880bbd5&response_type=code&redirect_uri=http://localhost:8765/individual_user_consent/&response_mode=query&scope=https%3A%2F%2Fgraph.microsoft.com%2Fcalendars.read%20https%3A%2F%2Fgraph.microsoft.com%2Fmail.send&state=12345")
+	})
+
+	r.GET("/individual_user_consent/", func(c *gin.Context) {
+		responseCode := c.Query("code")
+		log.Println("code", responseCode)
+		if responseCode != "" {
+			code = responseCode
+			c.Redirect(http.StatusFound, "https://login.microsoftonline.com/afae2f63-1bcb-4d1f-b8c3-252a4cd3dd07/v2.0/adminconsent?client_id=46442420-1b26-4bd7-a997-183e1880bbd5&state=12345&redirect_uri=http://localhost:8765/individual_user_consent/&scope=https://graph.microsoft.com/calendars.read%20https://graph.microsoft.com/mail.send")
+			return
+		}
+		admin_consent := c.Query("admin_consent")
+		log.Println("admin_consent:", admin_consent)
+		state := c.Query("state")
+		log.Println("state:", state)
+		scope := c.Query("scope")
+		log.Println("scope:", scope)
+
+		req, err := http.NewRequest("POST", "https://login.microsoftonline.com/afae2f63-1bcb-4d1f-b8c3-252a4cd3dd07/oauth2/v2.0/token",
+			strings.NewReader(`client_secret=msHRpSOTQLP24lCk9afnSTejW%3DlV%3F8%3D%40&grant_type=authorization_code&client_id=46442420-1b26-4bd7-a997-183e1880bbd5&scope=https://outlook.office.com/mail.read https://outlook.office.com/mail.send&redirect_uri=http://localhost:8765/individual_user_consent/&code=`+code))
+		if err != nil {
+			log.Fatal(err)
+		}
+		client := http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer resp.Body.Close()
+		respBody, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		var response gin.H
+		if err = json.Unmarshal(respBody, &response); err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, &response)
+	})
+
+	bind := os.Getenv("BINDADDR")
+	if bind == "" {
+		bind = "127.0.0.1:8765"
+	}
+	log.Fatal(r.Run(bind))
+}
